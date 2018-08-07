8930cd9674fbf5c934350028d037596e0442b28f
Sebastian Speitel <sebastian.speitel@outlook.de>
2018-08-07 10:32:30 +0200

    added LEDStrip

---
+++ LEDStrip.js
@@ -0,0 +1,41 @@
+﻿const chalk = require('chalk');
+
+let ws281x;
+try {
+    ws281x = require('rpi-ws281x-native');
+} catch (er) {
+    ws281x = null;
+}
+
+class LEDStrip {
+    constructor(cnt = 1, toConsole = false) {
+        this.cnt = cnt || 1;
+        this.pixel = new Uint32Array(this.cnt);
+        this.pixelRGB = Array(this.cnt).fill({ r: 0, g: 0, b: 0 });
+        this.toConsole = toConsole;
+        if (ws281x) ws281x.init(this.NUM_LEDS);
+    }
+
+    set(i, r, g, b, update = true) {
+        if (i < 0 || i >= this.pixel.length) throw new Error('IndexOutOfBounds');
+        r = Math.round(Math.min(Math.max(0, r), 255));
+        g = Math.round(Math.min(Math.max(0, g), 255));
+        b = Math.round(Math.min(Math.max(0, b), 255));
+        
+        let c = ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+        this.pixelRGB[i] = { r: r, g: g, b: b };
+        this.pixel[i] = c;
+        if (update) this.update();
+    }
+
+    get text() {
+        return this.pixelRGB.reduce((s, c) => s += chalk.rgb(c.r, c.g, c.b)('█'), '');
+    }
+
+    update() {
+        if (ws281x) ws281x.render(this.pixel);
+        if (this.toConsole) console.log(this.text);
+    }
+}
+
+module.exports = LEDStrip;
--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 'use strict';
 const wD = require('./weatherData.js');
+const leds = new (require('./LEDStrip.js'))(60);
 
+wD.loaded.then(main).catch(err => console.log(err));
 const locations = new Set();
 
-wD.loaded.then(() => {
+function main() {
 
     console.log('starting app.js');
     locations.add(new wD.Location(707860, { mode: '>>' }));
     locations.add(new wD.Location(2878044, { mode: '>>' }));
     locations.add(new wD.Location(803611, { mode: '>>' }));
     locations.add(new wD.Location(874652, { mode: '>>' }));
-    locations.add(new wD.Location(2347078, { mode: '>>' })); 
+    locations.add(new wD.Location(2347078, { mode: '>>' }));
     locations.add(new wD.Location(2822542, { mode: '>>' })); //Thüringen
     locations.add(new wD.Location(2896514, { mode: '>>' })); //Ilmenau
     //setTimeout(() => {
     //    locations.forEach(l => console.log(l.weather()));
 
     //}, 1000);
+    let i = 0;
+    locations.forEach((l) => {
+        let w = l.weather();
+        leds.set(i++, 0, 0, w.hum);
+    });
+    console.log(`LEDS:${leds.text}`);
 
-}).catch(err => console.log(err));
+
+}
+
